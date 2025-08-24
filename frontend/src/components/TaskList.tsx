053c8a273dc129c3537839a0d527a188f2948030
Ivan <picea.abies10@gmail.com>
2025-08-25 00:24:59 +0300

    Fix styles .

--- frontend/src/components/TaskList.tsx
+++ frontend/src/components/TaskList.tsx
@@ -23,41 +23,45 @@ export default function TaskList({
             selectedTaskId === task.id ? 'border-blue-800 bg-blue-100' : ''
           }`}
           onClick={() => onEdit(task)}>
-          <div>
-            <h3 className="font-light">
-              {task.done ? <s>{task.title}</s> : task.title}
-            </h3>
-            {task.description && (
-              <p className="text-sm text-gray-400">{task.description}</p>
-            )}
+          <div className="flex items-center space-x-4">
+            <div>
+              <label
+                className="relative cursor-pointer h-4 w-4 flex items-center"
+                onClick={e => e.stopPropagation()}>
+                <input
+                  type="checkbox"
+                  checked={task.done}
+                  onChange={() => onUpdate(task.id, { done: !task.done })}
+                  className="peer absolute opacity-0 h-12 w-12 cursor-pointer"
+                />
+                <span className="block h-4 w-4 border rounded-full bg-white peer-checked:bg-white peer-checked:border-gray-600 transition-colors">
+                  {task.done && (
+                    <svg
+                      className="absolute left-0 top-0 h-4 w-4 text-black"
+                      viewBox="0 0 16 16"
+                      fill="none">
+                      <path
+                        d="M4 8l3 3 5-5"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  )}
+                </span>
+              </label>
+            </div>
+            <div>
+              <h3 className="font-light">
+                {task.done ? <s>{task.title}</s> : task.title}
+              </h3>
+              {task.description && (
+                <p className="text-sm text-gray-400">{task.description}</p>
+              )}
+            </div>
           </div>
           <div className="flex items-center space-x-4">
-            <label
-              className="relative cursor-pointer h-4 w-4 flex items-center"
-              onClick={e => e.stopPropagation()}>
-              <input
-                type="checkbox"
-                checked={task.done}
-                onChange={() => onUpdate(task.id, { done: !task.done })}
-                className="peer absolute opacity-0 h-12 w-12 cursor-pointer"
-              />
-              <span className="block h-4 w-4 border rounded-full bg-white peer-checked:bg-white peer-checked:border-gray-600 transition-colors">
-                {task.done && (
-                  <svg
-                    className="absolute left-0 top-0 h-4 w-4 text-black"
-                    viewBox="0 0 16 16"
-                    fill="none">
-                    <path
-                      d="M4 8l3 3 5-5"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                )}
-              </span>
-            </label>
             <select
               value={task.priority}
               onChange={e =>
