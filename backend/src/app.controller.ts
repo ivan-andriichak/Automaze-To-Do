357d84dbb187b0c8f2378bb57ba2d6fab1c3ada6
Ivan <picea.abies10@gmail.com>
2025-08-25 22:05:08 +0300

    Fix problem.

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get()
+  @Get('/')
   getHealthStatus(): { status: string; message: string } {
     return {
       status: 'ok',
