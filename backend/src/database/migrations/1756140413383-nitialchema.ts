7d3cbf69656b6f8f96a794917b10cd1c584d5c09
Ivan <picea.abies10@gmail.com>
2025-08-25 21:01:23 +0300

    Refactor migration.

--- backend/src/database/migrations/1756140413383-nitialchema.ts
+++ backend/src/database/migrations/1756140413383-nitialchema.ts
@@ -5,15 +5,17 @@ export class Nitialchema1756140413383 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            CREATE TABLE "tasks" (
-                "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-                "title" character varying NOT NULL,
-                "description" character varying,
-                "done" boolean NOT NULL DEFAULT false,
-                "priority" integer NOT NULL DEFAULT 1,
-                CONSTRAINT "PK_tasks_id" PRIMARY KEY ("id")
-            )
-        `);
+      CREATE TABLE "tasks" (
+                             "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+                             "title" character varying NOT NULL,
+                             "description" character varying,
+                             "done" boolean NOT NULL DEFAULT false,
+                             "priority" integer NOT NULL DEFAULT 1,
+                             "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+                             "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
+                             CONSTRAINT "PK_tasks_id" PRIMARY KEY ("id")
+      )
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
