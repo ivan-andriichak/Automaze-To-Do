b93cbf46478fa368086ef42614e34998f3929cfb
Ivan <picea.abies10@gmail.com>
2025-08-26 15:29:51 +0300

    Fix style .

--- frontend/src/components/icons/CheckIcon.tsx
+++ frontend/src/components/icons/CheckIcon.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-
-type IconProps = React.SVGProps<SVGSVGElement>;
-
-export default function CheckIcon(props: IconProps) {
+export default function CheckIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox="0 0 16 16" fill="none" {...props}>
-      <path
-        d="M4 8l3 3 5-5"
+      <polyline
+        points="4,9 7,12 12,5"
         stroke="currentColor"
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
+        fill="none"
       />
     </svg>
   );
