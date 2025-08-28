5533daceb73c728b64babfbbbce3094d21443666
Ivan <picea.abies10@gmail.com>
2025-08-28 12:37:24 +0300

    Production with SSL.

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -3,20 +3,21 @@ import * as dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
 import getter from './src/config/configuration';
 
-dotenv.config({ path: './environments/local.env' });
-
 const isProduction = process.env.NODE_ENV === 'production';
+
+if (!isProduction) {
+  dotenv.config({ path: './environments/local.env' });
+}
+
 const databaseConfig = getter().postgres;
 
 console.log('ormconfig.ts Config:', {
-  host: isProduction ? 'Render DB' : databaseConfig.host,
-  port: isProduction ? undefined : databaseConfig.port,
-  username: isProduction ? undefined : databaseConfig.user,
-  password: isProduction ? undefined : databaseConfig.password,
-  database: isProduction ? undefined : databaseConfig.dbName,
+  host: databaseConfig.host,
+  port: databaseConfig.port,
+  username: databaseConfig.user,
+  password: databaseConfig.password,
+  database: databaseConfig.dbName,
   POSTGRES_URL: process.env.POSTGRES_URL,
-  NODE_ENV: process.env.NODE_ENV,
-  ssl: isProduction ? { rejectUnauthorized: false } : undefined,
 });
 
 if (isProduction && !process.env.POSTGRES_URL) {
@@ -32,23 +33,22 @@ if (
 
 export default new DataSource({
   type: 'postgres',
-  url: isProduction ? process.env.POSTGRES_URL : undefined,
-  host: isProduction ? undefined : databaseConfig.host,
-  port: isProduction ? undefined : databaseConfig.port,
-  username: isProduction ? undefined : databaseConfig.user,
-  password: isProduction ? undefined : databaseConfig.password,
-  database: isProduction ? undefined : databaseConfig.dbName,
+  ...(isProduction
+    ? {
+        url: process.env.POSTGRES_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: databaseConfig.host,
+        port: databaseConfig.port,
+        username: databaseConfig.user,
+        password: databaseConfig.password,
+        database: databaseConfig.dbName,
+      }),
   entities: [path.join(__dirname, 'src', 'database', 'entities', '*.entity{.ts,.js}')],
-  migrations: [path.join(__dirname, 'src', 'database', 'migrations', '*.{ts,.js}')],
+  migrations: [path.join(__dirname, 'src', 'database', 'migrations', '*.{ts,js}')],
   synchronize: false,
   dropSchema: false,
   logging: true,
   migrationsRun: isProduction,
-  extra: isProduction
-    ? {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    }
-    : {},
 });
