6afc0413201653d69a114616250754b93290f288
Ivan <picea.abies10@gmail.com>
2025-08-28 13:40:58 +0300

    Production with SSL.

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -1,11 +1,34 @@
 import * as path from 'node:path';
 import * as dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
+import getter from './src/config/configuration';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 if (!isProduction) {
   dotenv.config({ path: './environments/local.env' });
+}
+
+const databaseConfig = getter().postgres;
+
+console.log('ormconfig.ts Config:', {
+  host: databaseConfig.host,
+  port: databaseConfig.port,
+  username: databaseConfig.user,
+  password: databaseConfig.password,
+  database: databaseConfig.dbName,
+  POSTGRES_URL: process.env.POSTGRES_URL,
+});
+
+if (isProduction && !process.env.POSTGRES_URL) {
+  throw new Error('Missing POSTGRES_URL in production environment');
+}
+
+if (
+  !isProduction &&
+  (!databaseConfig.host || !databaseConfig.user || !databaseConfig.password || !databaseConfig.dbName)
+) {
+  throw new Error('Missing PostgreSQL configuration in ormconfig.ts for non-production');
 }
 
 export default new DataSource({
@@ -16,11 +39,11 @@ export default new DataSource({
         ssl: { rejectUnauthorized: false },
       }
     : {
-        host: process.env.POSTGRES_HOST,
-        port: Number(process.env.POSTGRES_PORT),
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DATABASE,
+        host: databaseConfig.host,
+        port: databaseConfig.port,
+        username: databaseConfig.user,
+        password: databaseConfig.password,
+        database: databaseConfig.dbName,
       }),
   entities: [path.join(__dirname, 'src', 'database', 'entities', '*.entity{.ts,.js}')],
   migrations: [path.join(__dirname, 'src', 'database', 'migrations', '*.{ts,js}')],
--- backend/src/modules/postgres/postgres.module.ts
+++ backend/src/modules/postgres/postgres.module.ts
@@ -4,27 +4,40 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { Config, PostgresConfig } from '../../config/config.type';
+import { Config } from '../../config/config.type';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useFactory: async (configService: ConfigService<Config>) => {
-        const config = configService.get<PostgresConfig>('postgres');
-
-        return {
-          type: 'postgres',
-          host: config?.host,
-          port: config?.port,
-          username: config?.user,
-          password: config?.password,
-          database: config?.dbName,
-          entities: [path.join(__dirname, '..', '..', 'database', 'entities', '*.entity.{js,ts}')],
-          migrations: [path.join(__dirname, '..', '..', 'database', 'migrations', '*.{js,ts}')],
-          migrationsRun: true,
-          synchronize: false,
-          logging: true,
-        };
+        const isProduction = process.env.NODE_ENV === 'production';
+        if (isProduction) {
+          return {
+            type: 'postgres',
+            url: process.env.POSTGRES_URL,
+            ssl: { rejectUnauthorized: false },
+            entities: [path.join(__dirname, '..', '..', 'database', 'entities', '*.entity.{js,ts}')],
+            migrations: [path.join(__dirname, '..', '..', 'database', 'migrations', '*.{js,ts}')],
+            migrationsRun: true,
+            synchronize: false,
+            logging: true,
+          };
+        } else {
+          const config = configService.get('postgres');
+          return {
+            type: 'postgres',
+            host: config?.host,
+            port: config?.port,
+            username: config?.user,
+            password: config?.password,
+            database: config?.dbName,
+            entities: [path.join(__dirname, '..', '..', 'database', 'entities', '*.entity.{js,ts}')],
+            migrations: [path.join(__dirname, '..', '..', 'database', 'migrations', '*.{js,ts}')],
+            migrationsRun: true,
+            synchronize: false,
+            logging: true,
+          };
+        }
       },
       inject: [ConfigService],
     }),
